c3f8cbfdbe4f1c8b8de4f668f5f46285ab1cddef
hossein <hosseinsiadati01@gmail.com>
2025-03-30 08:28:37 +0330

    project dockerized

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.12
+
+ENV PYTHONDONTWRITEBYCODE=1
+# ENV key=value
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+EXPOSE 8000
+COPY . . 
--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import PaymentModel
 
-# Register your models here.
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_filter = ['status', 'user', 'amount']
+    list_display = ['user', "amount", "status",]
+    search_fields = ['user', 'status', 'authority', 'ref_id']
+    ordering = 'user'
+    
+    
